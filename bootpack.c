0bb2d396e77e76a9a94ba642e3fabdecf428d0fe
Heppoko <yuhki.yasuda@gmail.com>
2018-04-14 23:03:33 +0900

    4日目 終了

--- bootpack.c
+++ bootpack.c
@@ -8,7 +8,7 @@ void _io_store_eflags(int eflags);
 /*** from C ***/
 void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
-void boxfill(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
 
 #define COL8_000000		0
 #define COL8_FF0000		1
@@ -29,16 +29,30 @@ void boxfill(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, in
 
 void HariMain(void)
 {
-    int i;
-    char *p;
+    char *vram;
+    int xsize, ysize;
 
     init_palette();
+    vram = (char *) 0xa0000;
+    xsize = 320;
+    ysize = 200;
 
-    p = (char *) 0xa0000;
+    boxfill8(vram, xsize, COL8_008484,  0,         0,          xsize -  1, ysize - 29);
+	boxfill8(vram, xsize, COL8_C6C6C6,  0,         ysize - 28, xsize -  1, ysize - 28);
+	boxfill8(vram, xsize, COL8_FFFFFF,  0,         ysize - 27, xsize -  1, ysize - 27);
+	boxfill8(vram, xsize, COL8_C6C6C6,  0,         ysize - 26, xsize -  1, ysize -  1);
 
-    boxfill(p, 320, COL8_FF0000, 20, 20, 120, 120);
-    boxfill(p, 320, COL8_00FF00, 70, 50, 170, 150);
-    boxfill(p, 320, COL8_0000FF, 120, 80, 220, 180);
+	boxfill8(vram, xsize, COL8_FFFFFF,  3,         ysize - 24, 59,         ysize - 24);
+	boxfill8(vram, xsize, COL8_FFFFFF,  2,         ysize - 24,  2,         ysize -  4);
+	boxfill8(vram, xsize, COL8_848484,  3,         ysize -  4, 59,         ysize -  4);
+	boxfill8(vram, xsize, COL8_848484, 59,         ysize - 23, 59,         ysize -  5);
+	boxfill8(vram, xsize, COL8_000000,  2,         ysize -  3, 59,         ysize -  3);
+	boxfill8(vram, xsize, COL8_000000, 60,         ysize - 24, 60,         ysize -  3);
+
+	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize -  4, ysize - 24);
+	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize -  4);
+	boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize -  3, xsize -  4, ysize -  3);
+	boxfill8(vram, xsize, COL8_FFFFFF, xsize -  3, ysize - 24, xsize -  3, ysize -  3);
 
     for(;;) {
         _io_hlt();
@@ -85,7 +99,7 @@ void set_palette(int start, int end, unsigned char *rgb)
     return;
 }
 
-void boxfill(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1)
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1)
 {
     int x, y;
     for(y = y0; y <= y1; y++) {
