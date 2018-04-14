2de3ba072db7f94b71fa979ea74dde9d4cb747c7
Heppoko <yuhki.yasuda@gmail.com>
2018-04-14 22:40:52 +0900

    4日目 しましま〜（色違う)

--- bootpack.c
+++ bootpack.c
@@ -1,15 +1,68 @@
+/*** from assembly ***/
 void _io_hlt(void);
+void _io_cli(void);
+void _io_out8(int port, int data);
+int _io_load_eflags(void);
+void _io_store_eflags(int eflags);
+
+/*** from C ***/
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
 
 void HariMain(void)
 {
     int i;
     char *p;
 
-    for(i = 0xa0000; i <= 0xaffff; i++) {
-        p = i;
-        *p = i & 0x0f;
+    init_palette();
+
+    p = (char *) 0xa0000;
+
+    for(i = 0; i <= 0xffff; i++) {
+        p[i] = i & 0x0f;
     }
+
     for(;;) {
         _io_hlt();
     }
+}
+
+void init_palette(void)
+{
+    static unsigned char table_rgb[16 * 3] = {
+        0x00, 0x00, 0x00, //black
+        0xff, 0x00, 0x00, //bright red
+        0x00, 0xff, 0x00, //bright green
+        0xff, 0xff, 0x00, //bright yellow
+        0x00, 0x00, 0xff, //bright blue
+        0xff, 0x00, 0xff, //bright purple
+        0x00, 0xff, 0xff, //bright light blue
+        0xff, 0xff, 0xff, //white
+        0xc6, 0xc6, 0xc6, //bright grey
+        0x84, 0x00, 0x00, //dark red
+        0x00, 0x84, 0x00, //dark green
+        0x84, 0x84, 0x00, //dark yellow
+        0x00, 0x00, 0x84, //dark blue
+        0x84, 0x00, 0x84, //dark purple
+        0x00, 0x84, 0x84, //dark light blue
+        0x84, 0x84, 0x84  //dark gray
+    };
+    set_palette(0, 15, table_rgb);
+    return;
+}
+
+void set_palette(int start, int end, unsigned char *rgb)
+{
+    int i, eflags;
+    eflags = _io_load_eflags();
+    _io_cli();
+    _io_out8(0x03c8, start);
+    for(i = start; i <= end; i++) {
+        _io_out8(0x03c9, rgb[0] / 4);
+        _io_out8(0x03c9, rgb[1] / 4);
+        _io_out8(0x03c9, rgb[2] / 4);
+        rgb += 3;
+    }
+    _io_store_eflags(eflags);
+    return;
 }
--- nasmfunc.asm
+++ nasmfunc.asm
@@ -1,13 +1,68 @@
 section .text
-    GLOBAL _io_hlt
-    GLOBAL _write_mem8
+    GLOBAL _io_hlt, _io_cli, _io_sti, _io_stihlt
+    GLOBAL _io_in8, _io_in16, _io_in32
+    GLOBAL _io_out8, _io_out16, _io_out32
+    GLOBAL _io_load_eflags, _io_store_eflags
 
 _io_hlt:
     HLT
     RET
 
-_write_mem8:
-    MOV ECX,[ESP+4]
+_io_cli:
+    CLI
+    RET
+
+_io_sti:
+    STI
+    RET
+
+_io_stihlt:
+    STI
+    HLT
+    RET
+
+_io_in8:
+    MOV EDX,[ESP+4]
+    MOV EAX,0
+    IN AL,DX
+    RET
+
+_io_in16:
+    MOV EDX,[ESP+4]
+    MOV EAX,0
+    IN AX,DX
+    RET
+
+_io_in32:
+    MOV EDX,[ESP+4]
+    IN EAX,DX
+    RET
+
+_io_out8:
+    MOV EDX,[ESP+4]
     MOV AL,[ESP+8]
-    MOV [ECX],AL
+    OUT DX,AL
+    RET
+
+_io_out16:
+    MOV EDX,[ESP+4]
+    MOV EAX,[ESP+8]
+    OUT DX,AX
+    RET
+
+_io_out32:
+    MOV EDX,[ESP+4]
+    MOV EAX,[ESP+8]
+    OUT DX,EAX
+    RET
+
+_io_load_eflags:
+    PUSHFD
+    POP EAX
+    RET
+
+_io_store_eflags:
+    MOV EAX,[ESP+4]
+    PUSH EAX
+    POPFD
     RET
