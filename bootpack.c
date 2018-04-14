2de28edf670faed5ca351a310423bca785212450
Heppoko <yuhki.yasuda@gmail.com>
2018-04-14 22:57:33 +0900

    4日目 しかく〜

--- bootpack.c
+++ bootpack.c
@@ -8,6 +8,24 @@ void _io_store_eflags(int eflags);
 /*** from C ***/
 void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
+void boxfill(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+
+#define COL8_000000		0
+#define COL8_FF0000		1
+#define COL8_00FF00		2
+#define COL8_FFFF00		3
+#define COL8_0000FF		4
+#define COL8_FF00FF		5
+#define COL8_00FFFF		6
+#define COL8_FFFFFF		7
+#define COL8_C6C6C6		8
+#define COL8_840000		9
+#define COL8_008400		10
+#define COL8_848400		11
+#define COL8_000084		12
+#define COL8_840084		13
+#define COL8_008484		14
+#define COL8_848484		15
 
 void HariMain(void)
 {
@@ -18,9 +36,9 @@ void HariMain(void)
 
     p = (char *) 0xa0000;
 
-    for(i = 0; i <= 0xffff; i++) {
-        p[i] = i & 0x0f;
-    }
+    boxfill(p, 320, COL8_FF0000, 20, 20, 120, 120);
+    boxfill(p, 320, COL8_00FF00, 70, 50, 170, 150);
+    boxfill(p, 320, COL8_0000FF, 120, 80, 220, 180);
 
     for(;;) {
         _io_hlt();
@@ -64,5 +82,15 @@ void set_palette(int start, int end, unsigned char *rgb)
         rgb += 3;
     }
     _io_store_eflags(eflags);
+    return;
+}
+
+void boxfill(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1)
+{
+    int x, y;
+    for(y = y0; y <= y1; y++) {
+        for(x = x0; x <= x1; x++)
+            vram[y * xsize + x] = c;
+    }
     return;
 }
