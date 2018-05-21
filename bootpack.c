b37766cd21927db1596179b9dda4209a09780da9
Heppoko <yuhki.yasuda@gmail.com>
2018-05-21 15:03:07 +0900

    [update]分割コンパイルー!

--- bootpack.c
+++ bootpack.c
@@ -1,5 +1,6 @@
-#include "hankaku.c"
 #include "sprintf.c"
+#include "graphic.c"
+#include "dsctbl.c"
 #include <stdarg.h>
 
 /*** from assembly ***/
@@ -8,18 +9,24 @@ void _io_cli(void);
 void _io_out8(int port, int data);
 int _io_load_eflags(void);
 void _io_store_eflags(int eflags);
-void _load_idtr(int limit, int addr);
-void _load_gdtr(int limit, int addr);
 
 /*** from C ***/
-void init_palette(void);
-void set_palette(int start, int end, unsigned char *rgb);
-void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
-void init_screen(char *vram, int x, int y);
-void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
-void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
-void init_mouse_cursor8(char *mouse, char bc);
-void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0, int py0, char *buf, int bxsize);
+extern void init_palette(void);
+extern void set_palette(int start, int end, unsigned char *rgb);
+extern void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+extern void init_screen(char *vram, int x, int y);
+extern void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
+extern void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
+extern void init_mouse_cursor8(char *mouse, char bc);
+extern void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0, int py0, char *buf, int bxsize);
+
+struct BOOTINFO {
+    char cyls, leds, vmode, reserve;
+    short scrnx, scrny;
+    char *vram;
+};
+
+extern void sprintf(char *str, char *fmt, ...);
 
 #define COL8_000000		0
 #define COL8_FF0000		1
@@ -37,29 +44,6 @@ void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0, int py
 #define COL8_840084		13
 #define COL8_008484		14
 #define COL8_848484		15
-
-struct BOOTINFO {
-    char cyls, leds, vmode, reserve;
-    short scrnx, scrny;
-    char *vram;
-};
-
-struct SEGMENT_DESCRIPTOR {
-    short limit_low, base_low;
-    char base_mid, access_right;
-    char limit_high, base_high; 
-};
-struct GATE_DESCRIPTOR {
-    short offset_low, selector;
-    char dw_count, access_right;
-    short offset_high;
-};
-
-void init_gdtidt(void);
-void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
-void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
-extern char hankaku[4096];
-extern void sprintf(char *str, char *fmt, ...);
 
 void HariMain(void)
 {
@@ -79,192 +63,4 @@ void HariMain(void)
     for(;;) {
         _io_hlt();
     }
-}
-
-void init_palette(void)
-{
-    static unsigned char table_rgb[16 * 3] = {
-        0x00, 0x00, 0x00, //black
-        0xff, 0x00, 0x00, //bright red
-        0x00, 0xff, 0x00, //bright green
-        0xff, 0xff, 0x00, //bright yellow
-        0x00, 0x00, 0xff, //bright blue
-        0xff, 0x00, 0xff, //bright purple
-        0x00, 0xff, 0xff, //bright light blue
-        0xff, 0xff, 0xff, //white
-        0xc6, 0xc6, 0xc6, //bright grey
-        0x84, 0x00, 0x00, //dark red
-        0x00, 0x84, 0x00, //dark green
-        0x84, 0x84, 0x00, //dark yellow
-        0x00, 0x00, 0x84, //dark blue
-        0x84, 0x00, 0x84, //dark purple
-        0x00, 0x84, 0x84, //dark light blue
-        0x84, 0x84, 0x84  //dark gray
-    };
-    set_palette(0, 15, table_rgb);
-    return;
-}
-
-void set_palette(int start, int end, unsigned char *rgb)
-{
-    int i, eflags;
-    eflags = _io_load_eflags();
-    _io_cli();
-    _io_out8(0x03c8, start);
-    for(i = start; i <= end; i++) {
-        _io_out8(0x03c9, rgb[0] / 4);
-        _io_out8(0x03c9, rgb[1] / 4);
-        _io_out8(0x03c9, rgb[2] / 4);
-        rgb += 3;
-    }
-    _io_store_eflags(eflags);
-    return;
-}
-
-void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1)
-{
-    int x, y;
-    for(y = y0; y <= y1; y++) {
-        for(x = x0; x <= x1; x++)
-            vram[y * xsize + x] = c;
-    }
-    return;
-}
-
-void init_screen(char *vram, int x, int y)
-{
-	boxfill8(vram, x, COL8_008484,  0,     0,      x -  1, y - 29);
-	boxfill8(vram, x, COL8_C6C6C6,  0,     y - 28, x -  1, y - 28);
-	boxfill8(vram, x, COL8_FFFFFF,  0,     y - 27, x -  1, y - 27);
-	boxfill8(vram, x, COL8_C6C6C6,  0,     y - 26, x -  1, y -  1);
-
-	boxfill8(vram, x, COL8_FFFFFF,  3,     y - 24, 59,     y - 24);
-	boxfill8(vram, x, COL8_FFFFFF,  2,     y - 24,  2,     y -  4);
-	boxfill8(vram, x, COL8_848484,  3,     y -  4, 59,     y -  4);
-	boxfill8(vram, x, COL8_848484, 59,     y - 23, 59,     y -  5);
-	boxfill8(vram, x, COL8_000000,  2,     y -  3, 59,     y -  3);
-	boxfill8(vram, x, COL8_000000, 60,     y - 24, 60,     y -  3);
-
-	boxfill8(vram, x, COL8_848484, x - 47, y - 24, x -  4, y - 24);
-	boxfill8(vram, x, COL8_848484, x - 47, y - 23, x - 47, y -  4);
-	boxfill8(vram, x, COL8_FFFFFF, x - 47, y -  3, x -  4, y -  3);
-	boxfill8(vram, x, COL8_FFFFFF, x -  3, y - 24, x -  3, y -  3);
-	return;
-}
-
-void putfont8(char *vram, int xsize, int x, int y, char c, char *font)
-{
-    int i;
-    char *p, d;
-    for(i = 0; i < 16; i++) {
-        p = vram + (y + i) * xsize + x;
-        d = font[i];
-        if((d & 0x80) != 0) { p[0] = c; }
-        if((d & 0x40) != 0) { p[1] = c; }
-        if((d & 0x20) != 0) { p[2] = c; }
-        if((d & 0x10) != 0) { p[3] = c; }
-        if((d & 0x08) != 0) { p[4] = c; }
-        if((d & 0x04) != 0) { p[5] = c; }
-        if((d & 0x02) != 0) { p[6] = c; }
-        if((d & 0x01) != 0) { p[7] = c; }
-    }
-    return;
-}
-
-void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s)
-{
-    for(; *s != 0x00; s++) {
-        putfont8(vram, xsize, x, y, c, hankaku + *s * 16);
-        x += 8;
-    }
-    return;
-}
-
-void init_mouse_cursor8(char *mouse, char bc)
-{
-    static char cursor[16][16] = {
-        "**************..",
-        "*OOOOOOOOOOO*...",
-        "*OOOOOOOOOO*....",
-        "*OOOOOOOOO*.....",
-        "*OOOOOOOO*......",
-        "*OOOOOOO*.......",
-        "*OOOOOOO*.......",
-        "*OOOOOOOO*......",
-        "*OOOO**OOO*.....",
-        "*OOO*..*OOO*....",
-        "*OO*....*OOO*...",
-        "*O*......*OOO*..",
-        "**........*OOO*.",
-        "*..........*OOO*",
-        "............*OO*",
-        ".............***"
-    };
-
-    int x, y;
-
-    for(y = 0; y < 16; y++) {
-        for(x = 0; x < 16; x++) {
-            if(cursor[y][x] == '*') mouse[y * 16 + x] = COL8_000000;
-            if(cursor[y][x] == 'O') mouse[y * 16 + x] = COL8_FFFFFF;
-            if(cursor[y][x] == '.') mouse[y * 16 + x] = bc;
-        }
-    }
-    return;
-}
-
-
-void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0, int py0, char *buf, int bxsize)
-{
-    int x, y;
-    for(y = 0; y < pysize; y++) {
-        for(x = 0; x < pxsize; x++) {
-            vram[(py0 + y) * vxsize + (px0 + x)] = buf[y * bxsize + x];
-        }
-    }
-    return;
-}
-
-void init_gdtidt(void)
-{
-    struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *) 0x00270000;
-    struct GATE_DESCRIPTOR    *idt = (struct GATE_DESCRIPTOR    *) 0x0026f800;
-    int i;
-
-    /*initalize GDT*/
-    for(i = 0; i < 8192; i++) set_segmdesc(gdt + i, 0, 0, 0);
-    set_segmdesc(gdt + 1, 0xffffffff, 0x00000000, 0x4092);
-    set_segmdesc(gdt + 2, 0x0007ffff, 0x00280000, 0x409a);
-    _load_gdtr(0xffff, 0x00270000);
-
-    /*initalize IDT*/
-    for(i = 0; i < 256; i++) set_gatedesc(idt + i, 0, 0, 0);
-    _load_idtr(0x7ff, 0x0026f800);
-
-    return;
-}
-
-void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar)
-{
-    if(limit > 0xffff) {
-        ar |= 0x8000;
-        limit /= 0x1000;
-    }
-    sd->limit_low = limit & 0xffff;
-    sd->base_low = base & 0xffff;
-    sd->base_mid = (base >> 16) & 0xff;
-    sd->access_right = ar & 0xff;
-    sd->limit_high = ((limit >> 16) & 0xff) | ((ar >> 8) & 0xf0);
-    sd->base_high = (base >> 24) & 0xff;
-    return;
-}
-
-void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar)
-{
-    gd->offset_low = offset & 0xffff;
-    gd->selector = selector;
-    gd->dw_count = (ar >> 8) & 0xff;
-    gd->access_right = ar & 0xff;
-    gd->offset_high = (offset >> 16) & 0xffff;
-    return;
 }
