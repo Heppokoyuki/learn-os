3b1c3cb0867745c4abe10fe4d04a088673742afa
Heppokoyuki <yuhki.yasuda@gmail.com>
2018-06-18 15:55:45 +0900

    重ね合わせの処理

--- bootpack.c
+++ bootpack.c
@@ -7,6 +7,9 @@ void HariMain(void)
 {
     struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
     struct MOUSE_DEC mdec;
+    struct SHTCTL *shtctl;
+    struct SHEET *sht_back, *sht_mouse;
+    unsigned char *buf_back, buf_mouse[256];
     char *mcursor, s[40], keybuf[32], mousebuf[128];
     unsigned int memtotal;
     struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
@@ -29,16 +32,26 @@ void HariMain(void)
     memman_free(memman, 0x00400000, memtotal - 0x00400000);
 
     init_palette();
-    init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
+    shtctl = shtctl_init(memman, binfo->vram, binfo->scrnx, binfo->scrny);
+    sht_back = sheet_alloc(shtctl);
+    sht_mouse = sheet_alloc(shtctl);
+    buf_back = (unsigned char *) memman_alloc_4k(memman, binfo->scrnx * binfo->scrny);
+    sheet_setbuf(sht_back, buf_back, binfo->scrnx, binfo->scrny, -1);
+    sheet_setbuf(sht_mouse, buf_mouse, 16, 16, 99);
+    init_screen8(buf_back, binfo->scrnx, binfo->scrny);
+    init_mouse_cursor8(buf_mouse, 99);
+    sheet_slide(shtctl, sht_back, 0, 0);
     mx = (binfo->scrnx - 16) / 2;
     my = (binfo->scrny - 28 - 16) / 2;
-    init_mouse_cursor8(mcursor, COL8_008484);
-    putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+    sheet_slide(shtctl, sht_mouse, mx, my);
+    sheet_updown(shtctl, sht_back, 0);
+    sheet_updown(shtctl, sht_mouse, 1);
     sprintf(s, "(%d, %d)", mx, my);
-    putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
+    putfonts8_asc(buf_back, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
     sprintf(s, "memory:%dMB  free:%dKB", memtotal / (1024 * 1024), memman_total(memman) / 1024);
-    putfonts8_asc(binfo->vram, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
+    putfonts8_asc(buf_back, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
+    sheet_refresh(shtctl, sht_back, 0, 0, binfo->scrnx, 48);
 
     for(;;) {
         _io_cli();
@@ -50,8 +63,9 @@ void HariMain(void)
                 i = fifo8_get(&keyfifo);
                 _io_sti();
                 sprintf(s, "%x", i);
-                boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
-                putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+                boxfill8(buf_back, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
+                putfonts8_asc(buf_back, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+                sheet_refresh(shtctl, sht_back, 0, 16, 16, 32);
             }
             else if(fifo8_status(&mousefifo) != 0) {
                 i = fifo8_get(&mousefifo);
@@ -67,9 +81,9 @@ void HariMain(void)
                     if((mdec.btn & 0x04) != 0) {
                         s[2] = 'C';
                     }
-                    boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 15 * 8 - 1, 31);
-                    putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
-                    boxfill8(binfo->vram, binfo->scrnx, COL8_008484, mx, my, mx + 15, my + 15);
+                    boxfill8(buf_back, binfo->scrnx, COL8_008484, 32, 16, 32 + 15 * 8 - 1, 31);
+                    putfonts8_asc(buf_back, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+                    sheet_refresh(shtctl, sht_back, 32, 16, 32 + 15 * 8, 32);
                     mx += mdec.x;
                     my += mdec.y;
                     if(mx < 0) mx = 0;
@@ -77,9 +91,10 @@ void HariMain(void)
                     if(mx > binfo->scrnx - 16) mx = binfo->scrnx - 16;
                     if(my > binfo->scrny - 16) my = binfo->scrny - 16;
                     sprintf(s, "(%d %d)", mx, my);
-                    boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 0, 79, 15);
-                    putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
-                    putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+                    boxfill8(buf_back, binfo->scrnx, COL8_008484, 0, 0, 79, 15);
+                    putfonts8_asc(buf_back, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
+                    sheet_refresh(shtctl, sht_back, 0, 0, 80, 16);
+                    sheet_slide(shtctl, sht_mouse, mx, my);
                 }
             }
         }
--- haribote.h
+++ haribote.h
@@ -58,6 +58,9 @@
 #define MEMMAN_FREES 4090
 #define MEMMAN_ADDR 0x003c0000
 
+#define MAX_SHEETS 256
+#define SHEET_USE 1
+
 #include <stdarg.h>
 
 struct BOOTINFO {
@@ -93,6 +96,16 @@ struct FREEINFO {
 struct MEMMAN {
     int frees, maxfrees, lostsize, losts;
     struct FREEINFO free[MEMMAN_FREES];
+};
+struct SHEET {
+    unsigned char *buf;
+    int bxsize, bysize, vx0, vy0, col_inv, height, flags;
+};
+struct SHTCTL {
+    unsigned char *vram;
+    int xsize, ysize, top;
+    struct SHEET *sheets[MAX_SHEETS];
+    struct SHEET sheets0[MAX_SHEETS];
 };
 
 /*** nasmfunc.asm ***/
@@ -154,6 +167,18 @@ void memman_init(struct MEMMAN *man);
 unsigned int memman_total(struct MEMMAN *man);
 unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
 int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
+unsigned int memman_alloc_4k(struct MEMMAN *man, unsigned int size);
+int memman_free_4k(struct MEMMAN *man, unsigned int addr, unsigned int size);
+
+/*** sheet.c ***/
+struct SHTCTL *shtctl_init(struct MEMMAN *memman, unsigned char *vram, int xsize, int ysize);
+struct SHEET *sheet_alloc(struct SHTCTL *ctl);
+void sheet_setbuf(struct SHEET *sht, unsigned char *buf, int xsize, int ysize, int col_inv);
+void sheet_updown(struct SHTCTL *ctl, struct SHEET *sht, int height);
+void sheet_refresh(struct SHTCTL *ctl, struct SHEET *sht, int bx0, int by0, int bx1, int by1);
+void sheet_slide(struct SHTCTL *ctl, struct SHEET *sht, int vx0, int vy0);
+void sheet_free(struct SHTCTL *ctl, struct SHEET *sht);
+void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1);
 
 /*** sprintf.c ***/
 int decimalAsciiConvert(char *str, int dec);
